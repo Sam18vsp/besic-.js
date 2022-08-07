0d8d3a205c3a99c10aa8859b9bb3ffdad400cd09
sam18v <>
2022-08-07 07:10:21 +0000

    Initial commit

---
+++ problem 3.js
@@ -0,0 +1,6 @@
+var side 1= ;
+var side 2= ;
+var side 3= ;
+var s = ( side1+ side2+ side3);
+var area=math.sqrt(s*((s-side1)*(s-side2)*(s-side3)));
+console.log (area)
---
+++ problem 5.js
@@ -0,0 +1,7 @@
+function cTf(celcious) 
+{
+  var cTemp = celcious ;
+  var cToFahr=cTemp 12;
+  var massage=cTemp+'\xBoc is '+cToFahr+'\xBof.';
+  console.log (massage)
+}
